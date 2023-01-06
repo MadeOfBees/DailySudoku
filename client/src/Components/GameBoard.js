a6d5711201b0c8828c934e945a6c762925084ef5
Bee <xronsredstone@gmail.com>
2023-01-05 20:42:05 -0500

    borked code

--- client/src/Components/GameBoard.js
+++ client/src/Components/GameBoard.js
@@ -1,79 +1,107 @@
 import * as React from 'react';
-import { Grid, Stack } from '@mui/material';
+import { Grid, Stack, Button, Modal, Box, Input } from '@mui/material';
 
-const GameBoard = (puzzle) => {
-    const [currentPuzzle] = React.useState(puzzle);
-    const DrawCell = ({number,chunk}) => {
-        const { puzzle } = currentPuzzle;
-        const currentChunk = (puzzle[chunk-1]);
-        const cell = currentChunk[number-1];
+const GameBoard = (props) => {
+    const style = props.style;
+    const puzzleWithShownVal = props.puzzle.map((chunk) => { return chunk.map((cell) => { return { ...cell, shownAs: cell.value }; }); });
+    const [currentPuzzle, setCurrentPuzzle] = React.useState({ puzzle: puzzleWithShownVal });
+    const [modalOpen, setModalOpen] = React.useState(false);
+    const [modalValue, setModalValue] = React.useState('');
+    const [currentCell, setCurrentCell] = React.useState({ chunk: 0, number: 0 });
+    React.useEffect(() => { drawBoard(currentPuzzle); }, [currentPuzzle]);
+
+    const DrawCell = ({ number, chunk }) => {
+        const shownPuzzle = currentPuzzle.puzzle;
+        const currentChunk = shownPuzzle[chunk - 1];
+        const cell = currentChunk[number - 1];
+        console.log(cell);
         if (cell.isCovered) {
-        return (
-          <div>
-            <h4>⠀</h4>
-          </div>
-        );}else{
-        return (
-          <div>
-            <h4>{cell.value}</h4>
-          </div>
-        );
+            return (
+                <div>
+                    <Button style={{ padding: '30%' }} onClick={() => {
+                        handleButtonClick(number - 1, chunk - 1);
+                    }}>⠀</Button>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <Button style={{ padding: '30%' }}>{cell.shownAs}</Button>
+                </div>
+            );
         }
-    }
+    };
 
-    const DrawChunk = ({number}) => {
+    const DrawChunk = ({ number }) => {
         const grids = [];
         for (let i = 0; i < 3; i++) {
-          grids.push(
-            <Grid key={i} container spacing={1}>
-              <Grid item xs={4}>
-                <DrawCell number={number} chunk={(i*3)+1}/>
-              </Grid>
-              <Grid item xs={4}>
-                <DrawCell number={number} chunk={(i*3)+2}/>
-              </Grid>
-              <Grid item xs={4}>
-                <DrawCell number={number} chunk={(i*3)+3}/>
-              </Grid>
-            </Grid>
-          );
+            grids.push(
+                <Grid key={i} container spacing={1}>
+                    <Grid item xs={4}>
+                        <DrawCell number={number} chunk={i * 3 + 1} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <DrawCell number={number} chunk={i * 3 + 2} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <DrawCell number={number} chunk={i * 3 + 3} />
+                    </Grid>
+                </Grid>
+            );
         }
-        return (
-          <Stack key={number}>
-            {grids}
-          </Stack>
-        );
-      }      
-    
-      const drawBoard = () => {
+        return <Stack key={number}>{grids}</Stack>;
+    };
+
+    const drawBoard = () => {
         const chunks = [];
         for (let i = 0; i < 3; i++) {
-          chunks.push(
-            <Grid key={i} container spacing={1}>
-              <Grid item xs={4}>
-                <DrawChunk number={(i*3)+1}/>
-              </Grid>
-              <Grid item xs={4}>
-                <DrawChunk number={(i*3)+2}/>
-              </Grid>
-              <Grid item xs={4}>
-                <DrawChunk number={(i*3)+3}/>
-              </Grid>
-            </Grid>
-          );
+            chunks.push(
+                <Grid key={i} container spacing={1}>
+                    <Grid item xs={4}>
+                        <DrawChunk number={i * 3 + 1} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <DrawChunk number={i * 3 + 2} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <DrawChunk number={i * 3 + 3} />
+                    </Grid>
+                </Grid>
+            );
         }
-        return (
-          <Stack>
-            {chunks}
-          </Stack>
-        );
-      }         
+        return <Stack>{chunks}</Stack>;
+    };
+
+    const handleModalSubmit = () => {
+        const currentCellNumber = currentCell.number;
+        const currentCellChunk = currentCell.chunk;
+        const newPuzzle = currentPuzzle.puzzle;
+        newPuzzle[currentCellChunk][currentCellNumber].shownAs = modalValue;
+        setCurrentPuzzle({ puzzle: newPuzzle });
+        setModalValue("");
+        setModalOpen(false);
+    };
+
+    const handleModalClose = () => {
+        setModalValue("");
+        setModalOpen(false);
+    };
+
+
+    const handleButtonClick = (number, chunk) => {
+        setModalOpen(true);
+        setCurrentCell({ number: number, chunk: chunk });
+    };
 
     return (
-        <Stack>
+        <div>
             {drawBoard(currentPuzzle)}
-        </Stack>
-    )
-}
-
-export default GameBoard;
+            <Modal open={modalOpen} onClose={handleModalClose}>
+                <Box sx={style}>
+                    <Input value={modalValue} onChange={(event) => setModalValue(event.target.value)} />
+                    <Button onClick={handleModalSubmit}>Submit</Button>
+                </Box>
+            </Modal>
+        </div>
+    );
+};
--- client/src/Pages/MainApp.js
+++ client/src/Pages/MainApp.js
@@ -1,21 +1,23 @@
 import * as React from 'react';
 import GameBoard from '../Components/GameBoard';
+const style = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4, };
+
 
 export default function MainApp() {
     const puzzle = [
         [{isCovered:false, value: 5 }, {isCovered:false, value: 3 }, {isCovered:false, value: 4 }, {isCovered:false, value: 6 }, {isCovered:false, value: 7 }, {isCovered:false, value: 8 }, {isCovered:false, value: 9 }, {isCovered:false, value: 1 }, {isCovered:false, value: 2 }],
-        [{isCovered:false, value: 6 }, {isCovered:false, value: 7 }, {isCovered:false, value: 2 }, {isCovered:false, value: 1 }, {isCovered:false, value: 9 }, {isCovered:false, value: 5 }, {isCovered:false, value: 3 }, {isCovered:false, value: 4 }, {isCovered:false, value: 8 }],
-        [{isCovered:false, value: 1 }, {isCovered:false, value: 9 }, {isCovered:false, value: 8 }, {isCovered:false, value: 3 }, {isCovered:false, value: 4 }, {isCovered:false, value: 2 }, {isCovered:false, value: 5 }, {isCovered:false, value: 6 }, {isCovered:false, value: 7 }],
+        [{isCovered:false, value: 6 }, {isCovered:false, value: 7 }, {isCovered:false, value: 2 }, {isCovered:false, value: 1 }, {isCovered:false, value: 9 }, {isCovered:false, value: 5 }, {isCovered:true, value: 3 }, {isCovered:false, value: 4 }, {isCovered:false, value: 8 }],
+        [{isCovered:false, value: 1 }, {isCovered:false, value: 9 }, {isCovered:false, value: 8 }, {isCovered:true, value: 3 }, {isCovered:false, value: 4 }, {isCovered:false, value: 2 }, {isCovered:false, value: 5 }, {isCovered:false, value: 6 }, {isCovered:false, value: 7 }],
         [{isCovered:false, value: 8 }, {isCovered:false, value: 5 }, {isCovered:false, value: 9 }, {isCovered:false, value: 7 }, {isCovered:false, value: 6 }, {isCovered:false, value: 1 }, {isCovered:false, value: 4 }, {isCovered:false, value: 2 }, {isCovered:false, value: 3 }],
         [{isCovered:false, value: 4 }, {isCovered:false, value: 2 }, {isCovered:false, value: 6 }, {isCovered:false, value: 8 }, {isCovered:true, value: 5 }, {isCovered:false, value: 3 }, {isCovered:false, value: 7 }, {isCovered:false, value: 9 }, {isCovered:false, value: 1 }],
         [{isCovered:false, value: 7 }, {isCovered:false, value: 1 }, {isCovered:false, value: 3 }, {isCovered:false, value: 9 }, {isCovered:false, value: 2 }, {isCovered:false, value: 4 }, {isCovered:false, value: 8 }, {isCovered:false, value: 5 }, {isCovered:false, value: 6 }],
-        [{isCovered:false, value: 9 }, {isCovered:false, value: 6 }, {isCovered:false, value: 1 }, {isCovered:false, value: 5 }, {isCovered:false, value: 3 }, {isCovered:false, value: 7 }, {isCovered:false, value: 2 }, {isCovered:false, value: 8 }, {isCovered:false, value: 4 }],
+        [{isCovered:true, value: 9 }, {isCovered:false, value: 6 }, {isCovered:false, value: 1 }, {isCovered:true, value: 5 }, {isCovered:false, value: 3 }, {isCovered:false, value: 7 }, {isCovered:false, value: 2 }, {isCovered:false, value: 8 }, {isCovered:false, value: 4 }],
         [{isCovered:false, value: 2 }, {isCovered:false, value: 8 }, {isCovered:false, value: 7 }, {isCovered:false, value: 4 }, {isCovered:false, value: 1 }, {isCovered:false, value: 9 }, {isCovered:false, value: 6 }, {isCovered:false, value: 3 }, {isCovered:false, value: 5 }],
         [{isCovered:false, value: 3 }, {isCovered:false, value: 4 }, {isCovered:false, value: 5 }, {isCovered:false, value: 2 }, {isCovered:false, value: 8 }, {isCovered:false, value: 6 }, {isCovered:false, value: 1 }, {isCovered:false, value: 7 }, {isCovered:false, value: 9 }]
         ]
     return (
         <div >
-            <GameBoard puzzle={puzzle} />
+            <GameBoard style={style} puzzle={puzzle} />
         </div>
     );
 }
