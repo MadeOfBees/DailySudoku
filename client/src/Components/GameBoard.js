4e9ca99fb9ca4c59dbc1e80913b035c47ff3d5a6
Bee <xronsredstone@gmail.com>
2023-01-13 16:39:47 -0500

    unborked

--- client/src/Components/GameBoard.js
+++ client/src/Components/GameBoard.js
@@ -1,96 +1,42 @@
 import * as React from 'react';
-import { Grid, Stack, Button, Modal, Box, Input } from '@mui/material';
+import {Button, Modal, Box, Input } from '@mui/material';
 
 const GameBoard = (props) => {
     const style = props.style;
-    const puzzleWithShownVal = props.puzzle.map((chunk) => { return chunk.map((cell) => { return { ...cell, shownAs: cell.value }; }); });
-    const [currentPuzzle, setCurrentPuzzle] = React.useState({ puzzle: puzzleWithShownVal });
+    const puzzleWithShownVal = props.puzzle.map((row) => {return row.map((cell) => {return {shownValue: cell.isShown ? cell.value : '%',trueValue: cell.value,};});});
+    const [currentPuzzle] = React.useState(puzzleWithShownVal);
     const [modalOpen, setModalOpen] = React.useState(false);
     const [modalValue, setModalValue] = React.useState('');
-    const [currentCell, setCurrentCell] = React.useState({ chunk: 0, number: 0 });
-    React.useEffect(() => { drawBoard(currentPuzzle); }, [currentPuzzle]);
-
-    const DrawCell = ({ number, chunk }) => {
-        const shownPuzzle = currentPuzzle.puzzle;
-        const currentChunk = shownPuzzle[chunk - 1];
-        const cell = currentChunk[number - 1];
-        console.log(cell);
-        if (cell.isCovered) {
-            return (
-                <div>
-                    <Button style={{ padding: '30%' }} onClick={() => {
-                        handleButtonClick(number - 1, chunk - 1);
-                    }}>⠀</Button>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <Button style={{ padding: '30%' }}>{cell.shownAs}</Button>
-                </div>
-            );
-        }
-    };
-
-    const DrawChunk = ({ number }) => {
-        const grids = [];
-        for (let i = 0; i < 3; i++) {
-            grids.push(
-                <Grid key={i} container spacing={1}>
-                    <Grid item xs={4}>
-                        <DrawCell number={number} chunk={i * 3 + 1} />
-                    </Grid>
-                    <Grid item xs={4}>
-                        <DrawCell number={number} chunk={i * 3 + 2} />
-                    </Grid>
-                    <Grid item xs={4}>
-                        <DrawCell number={number} chunk={i * 3 + 3} />
-                    </Grid>
-                </Grid>
-            );
-        }
-        return <Stack key={number}>{grids}</Stack>;
-    };
-
-    const drawBoard = () => {
-        const chunks = [];
-        for (let i = 0; i < 3; i++) {
-            chunks.push(
-                <Grid key={i} container spacing={1}>
-                    <Grid item xs={4}>
-                        <DrawChunk number={i * 3 + 1} />
-                    </Grid>
-                    <Grid item xs={4}>
-                        <DrawChunk number={i * 3 + 2} />
-                    </Grid>
-                    <Grid item xs={4}>
-                        <DrawChunk number={i * 3 + 3} />
-                    </Grid>
-                </Grid>
-            );
-        }
-        return <Stack>{chunks}</Stack>;
-    };
-
-    const handleModalSubmit = () => {
-        const currentCellNumber = currentCell.number;
-        const currentCellChunk = currentCell.chunk;
-        const newPuzzle = currentPuzzle.puzzle;
-        newPuzzle[currentCellChunk][currentCellNumber].shownAs = modalValue;
-        setCurrentPuzzle({ puzzle: newPuzzle });
-        setModalValue("");
-        setModalOpen(false);
-    };
-
     const handleModalClose = () => {
         setModalValue("");
         setModalOpen(false);
     };
+    const drawCell = (cell, cellIndex) => {
+        return (
+            <Button
+                key={cellIndex}
+                onClick={() => {
+                    setModalOpen(true);
+                }}
+            >
+                {cell.shownValue}
+            </Button>
+        );
+    };
+    const drawBoard = (currentPuzzle) => {
+        return currentPuzzle.map((row, rowIndex) => {
+            return (
+                <div key={rowIndex}>
+                    {row.map((cell, cellIndex) => {
+                        return drawCell(cell, cellIndex);
+                    })}
+                </div>
+            );
+        });
+    };
 
+    const handleModalSubmit = () => {
 
-    const handleButtonClick = (number, chunk) => {
-        setModalOpen(true);
-        setCurrentCell({ number: number, chunk: chunk });
     };
 
     return (
@@ -105,3 +51,5 @@ const GameBoard = (props) => {
         </div>
     );
 };
+
+export default GameBoard;
