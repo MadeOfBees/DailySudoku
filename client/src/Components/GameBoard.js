6555ab25047608137df66ad9f9d3225c84c429f7
Bee <xronsredstone@gmail.com>
2023-01-13 17:38:55 -0500

    cleanup

--- client/src/Components/GameBoard.js
+++ client/src/Components/GameBoard.js
@@ -1,43 +1,39 @@
 import * as React from 'react';
-import {Button, Modal, Box, Input } from '@mui/material';
+import { Button, Modal, Box, Input } from '@mui/material';
+document.body.style.overflow = 'hidden';
 
 const GameBoard = (props) => {
     const style = props.style;
-    const puzzleWithShownVal = props.puzzle.map((row) => {return row.map((cell) => {return {shownValue: cell.isShown ? cell.value : '⠀',trueValue: cell.value,};});});
+    const puzzleWithShownVal = props.puzzle.map((row) => { return row.map((cell) => { return { shownValue: cell.isShown ? cell.value : '⠀', trueValue: cell.value, }; }); });
     const [currentPuzzle] = React.useState(puzzleWithShownVal);
     const [modalOpen, setModalOpen] = React.useState(false);
     const [modalValue, setModalValue] = React.useState('');
-    document.body.style.overflow = 'hidden';
     const handleModalClose = () => {
         setModalValue("");
         setModalOpen(false);
     };
     const drawCell = (cell, cellIndex, rowIndex) => {
-        const cellName = `${String.fromCharCode(65 + rowIndex)}${cellIndex+1}`;
+        const cellName = `${String.fromCharCode(65 + rowIndex)}${cellIndex + 1}`;
         return (
-            <div key={cellIndex} className={cellName} style={{border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2.5REM', height : "5REM"}} onClick={() => {setModalOpen(true);}}>
+            <div key={cellIndex} id={cellName} style={{ border: '1px solid black', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2.5REM', height: "5REM" }} onClick={() => { cellChangeModal(cellName); }}>
                 {cell.shownValue}
             </div>
         );
     };
     const drawBoard = (currentPuzzle) => {
         return (
-            <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr', gridTemplateRows: '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr'}}>
-                {currentPuzzle.map((row, rowIndex) => {
-                    return row.map((cell, cellIndex) => {
-                        return (
-                            drawCell(cell, cellIndex, rowIndex)
-                        );
-                    });
-                })}
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr', gridTemplateRows: '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr' }}>
+                {currentPuzzle.map((row, rowIndex) => { return row.map((cell, cellIndex) => { return (drawCell(cell, cellIndex, rowIndex)); }); })}
             </div>
         );
     };
-    
+    const cellChangeModal = (cellName) => {
+        setModalOpen(true);
+        
+    };
     const handleModalSubmit = () => {
 
     };
-
     return (
         <div>
             {drawBoard(currentPuzzle)}
