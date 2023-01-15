3eb4f648097829e9dde654dc7d065bd31b16b860
Bee <xronsredstone@gmail.com>
2023-01-15 14:49:21 -0500

    cleanup

--- server/controllers/puzzle-controller.js
+++ server/controllers/puzzle-controller.js
@@ -1,8 +1,3 @@
-// import puzzle model
-const { User } = require('../models');
-// import sign token function from auth
-const { signToken } = require('../utils/auth');
-
 module.exports = {
   
 };
