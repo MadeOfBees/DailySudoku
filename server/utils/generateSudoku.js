c6516b8e2cc6813a94c66a90609f92726b827815
Bee <xronsredstone@gmail.com>
2023-01-16 16:01:59 -0500

    ladies, and gentlemen... (and other kind people)... we got em...

--- server/utils/generateSudoku.js
+++ server/utils/generateSudoku.js
@@ -1,83 +1,91 @@
-async function generateSudoku() {
-    let puzzle = "";
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            puzzle += ".";
-        }
-        puzzle += "\n";
+function generateSudoku(blankCount) {
+    let board = new Array(9).fill().map(() => new Array(9).fill(0));
+    if (backtrack(board)) {
+      let cells = [].concat(...board);
+      cells.sort(() => Math.random() - 0.5);
+      return formatStringsToObjects(board, blankCount);
     }
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            let num = Math.floor(Math.random() * 9) + 1;
-            while (!isValid(puzzle, i, j, num)) {
-                num = Math.floor(Math.random() * 9) + 1;
-            }
-            puzzle = updatePuzzle(puzzle, i, j, num);
-        }
-    }
-    puzzle = puzzle.replace(/(.)/g, '$1 ');
-    puzzle = puzzle.slice(0, -1);
-    return (genPuzzle(puzzle, creatPuzzWBlanks(puzzle, 10)));
-}
+    return false;
+  }
 
-function creatPuzzWBlanks(puzzle, blankCount) {
-    let puzzleNoBlank = puzzle.replace(/\s/g, '');
-    for (let i = 0; i < blankCount; i++) {
-        let rand = Math.floor(Math.random() * puzzleNoBlank.length);
-        while (puzzleNoBlank[rand] === 'X') {
-            rand = Math.floor(Math.random() * puzzleNoBlank.length);
+  function formatStringsToObjects(board, blankCount) {
+    // takes in a 2d array of strings and returns a 2d array of objects with a value and a boolean for whether or not the cell is shown to the user or not, pick those cells randomly
+    let cells = [].concat(...board);
+    cells.sort(() => Math.random() - 0.5);
+    let blankCells = cells.slice(0, blankCount);
+    let formattedBoard = board.map((row, rowIndex) => {
+        return row.map((cell, cellIndex) => {
+            return { value: cell, isShown: !blankCells.includes(cell) };
+        });
+    });
+    return formattedBoard;
+    }
+  
+  function backtrack(board) {
+    let emptyCell = findEmptyCell(board);
+    if (!emptyCell) {
+      return true;
+    }
+    let row = emptyCell.row;
+    let col = emptyCell.col;
+    for (let num = 1; num <= 9; num++) {
+      if (isValid(board, row, col, num)) {
+        board[row][col] = num;
+        if (backtrack(board)) {
+          return true;
         }
-        puzzleNoBlank = puzzleNoBlank.slice(0, rand) + 'X' + puzzleNoBlank.slice(rand + 1);
+        board[row][col] = 0;
+      }
     }
-    puzzleNoBlank = puzzleNoBlank.replace(/(.{9})/g, '$1\n');
-    puzzleNoBlank = puzzleNoBlank.replace(/(.)/g, '$1 ');
-    puzzleNoBlank = puzzleNoBlank.slice(0, -1);
-    console.log(puzzleNoBlank);
-    return puzzleNoBlank;
-}
-
-function isValid(puzzle, row, col, num) {
-    puzzle = puzzle.replace(/(.)/g, '$1 ');
-    let puzzleArr = puzzle.split("\n");
-    puzzleArr = puzzleArr.map((row) => row.split(" "));
-    const computeRow = puzzleArr[row]
-    if (computeRow.includes(num.toString())) {
-        return false;
-    }
-    // Check column
-    // Check box
-    return true;
-}
-
-function updatePuzzle(puzzle, row, col, num) {
-    let newPuzzle = puzzle.split('');
-    newPuzzle[row * 10 + col] = num;
-    return newPuzzle.join('');
-}
-
-function genPuzzle(solved, unsolved) {
-    let solvedArr = solved.split("\n");
-    let unsolvedArr = unsolved.split("\n");
-    solvedArr = solvedArr.map((row) => row.split(" "));
-    unsolvedArr = unsolvedArr.map((row) => row.split(" "));
-    solvedArr = solvedArr.map((row) => row.filter((val) => val !== ""));
-    unsolvedArr = unsolvedArr.map((row) => row.filter((val) => val !== ""));
-    solvedArr = solvedArr.map((row) => row.map((val) => parseInt(val)));
-    unsolvedArr = unsolvedArr.map((row) => row.map((val) => val));
-    let finalArr = [];
-    for (let i = 0; i < 9; i++) {
-        let innerArr = [];
-        for (let j = 0; j < 9; j++) {
-            innerArr.push({
-                value: solvedArr[i][j],
-                isBlank: (unsolvedArr[i][j] === 'X')
-            });
+    return false;
+  }
+  
+  function findEmptyCell(board) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (board[row][col] === 0) {
+          return {row, col};
         }
-        finalArr.push(innerArr);
+      }
     }
-    return finalArr;
-}
+    return false;
+  }
+  
+  function isValid(board, row, col, num) {
+    return !usedInRow(board, row, num) && !usedInCol(board, col, num) && !usedInBox(board, row - row % 3, col - col % 3, num);
+  }
+  
+  function usedInRow(board, row, num) {
+    for (let col = 0; col < 9; col++) {
+      if (board[row][col] == num) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function usedInCol(board, col, num) {
+    for (let row = 0; row < 9; row++) {
+      if (board[row][col] == num) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function usedInBox(board, boxStartRow, boxStartCol, num) {
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        if (board[row + boxStartRow][col + boxStartCol] == num) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  
 // Uncomment to generate new puzzle by hand
-generateSudoku().then(console.log);
+console.log(generateSudoku(10));
 
 module.exports = { generateSudoku };
