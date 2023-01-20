866d776c4f8383a7eb4653bdff315e4a2c1a7817
Bee <xronsredstone@gmail.com>
2023-01-20 15:10:18 -0500

    1

--- client/src/Pages/MainApp.js
+++ client/src/Pages/MainApp.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import GameBoard from '../Components/GameBoard';
 const style = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4, };
-const currentPuzzleURL = "http://localhost:3001/api/puzzles/current/"
 export default function MainApp() {
   const [puzzle, setPuzzle] = React.useState();
   React.useEffect(() => {
-    fetch(currentPuzzleURL)
+    fetch("http://localhost:3001/api/puzzles/current/")
       .then((res) => res.json())
       .then((data) => {
         if (data.message === "Puzzle retrieved successfully") {
