bd1504d9876fdf1019af595de434075676a2044e
Bee <xronsredstone@gmail.com>
2023-01-20 14:54:32 -0500

    ?

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/daily-puzzles', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/puzzle',
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 module.exports = mongoose.connection;
