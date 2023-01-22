c3d9bdf8e8677ae0db2f42dbfa058fbd1e90b113
Bee <xronsredstone@gmail.com>
2023-01-22 17:41:13 -0500

    nightly-server-add

--- server/controllers/puzzle-controller.js
+++ server/controllers/puzzle-controller.js
@@ -1,19 +1,25 @@
 const { generateSudoku } = require('../utils/generateSudoku');
 const { Puzzle } = require('../models/puzzle');
+require('dotenv').config();
 
 module.exports = {
     newPuzzle: async (req, res) => {
         try {
-            await Puzzle.deleteMany({});
+            if (req.body.password === process.env.SPASSWORD) {
             const puzzleArray = generateSudoku();
             const puzzleData = JSON.stringify(puzzleArray);
             const newPuzzle = new Puzzle({ puzzleData });
             await newPuzzle.save();
             res.status(201).json({ message: 'Puzzle created successfully', puzzle: newPuzzle });
+            }
+            else {
+                res.status(401).json({ message: 'Unauthorized' });
+            }
         } catch (error) {
             res.status(500).json({ message: 'Error creating puzzle', error });
         }
     },
+
     currentPuzzle: async (req, res) => {
         try {
             const puzzle = await Puzzle.findOne().sort({ _id: -1 });
@@ -25,24 +31,58 @@ module.exports = {
                 res.status(201).json({ message: 'Puzzle created successfully', puzzle: newPuzzle });
             }
             else {
-                const hourOfLastPuzzle = new Date(puzzle.createdAt).getUTCHours();
-                const hourOfLastPuzzleEST = hourOfLastPuzzle - 5 < 0 ? hourOfLastPuzzle + 19 : hourOfLastPuzzle - 5;
-                const timeForANewPuzzle = hourOfLastPuzzleEST < new Date().getUTCHours();
-                if (!timeForANewPuzzle) {
                     const puzzleArray = JSON.parse(puzzle.puzzleData);
                     res.status(200).json({ message: 'Puzzle retrieved successfully', puzzle: puzzleArray });
-                }
-                else {
-                    const puzzleArray = generateSudoku();
-                    const puzzleData = JSON.stringify(puzzleArray);
-                    const newPuzzle = new Puzzle({ puzzleData });
-                    await newPuzzle.save();
-                    res.status(202).json({ message: 'Puzzle created successfully', puzzle: newPuzzle });
-                }
             }
         } catch (error) {
             res.status(500).json({ message: 'Error retrieving puzzle', error });
         }
     },
 
+    getPuzzleByID: async (req, res) => {
+        try {
+            const puzzle = await Puzzle.findById(req.params.id);
+            const puzzleArray = JSON.parse(puzzle.puzzleData);
+            res.status(200).json({ message: 'Puzzle retrieved successfully', puzzle: puzzleArray });
+        }
+        catch (error) {
+            res.status(500).json({ message: 'Error retrieving puzzle', error });
+        }
+    },
+
+    getAllPuzzles: async (req, res) => {
+        try {
+            const puzzles = await Puzzle.find();
+            res.status(200).json({ message: 'Puzzles retrieved successfully', puzzles });
+        } catch (error) {
+            res.status(500).json({ message: 'Error retrieving puzzles', error });
+        }
+    },
+    
+    deletePuzzleByID: async (req, res) => {
+        try {
+            if (req.body.password === process.env.SPASSWORD) {
+            await Puzzle.findByIdAndDelete(req.params.id);
+            res.status(200).json({ message: 'Puzzle deleted successfully' });
+            }
+            else {
+                res.status(401).json({ message: 'Unauthorized' });
+            }
+        } catch (error) {
+            res.status(500).json({ message: 'Error deleting puzzle', error });
+        }
+    },
+    deleteAllPuzzles: async (req, res) => {
+        try {
+            if (req.body.password === process.env.SPASSWORD) {
+            await Puzzle.deleteMany();
+            res.status(200).json({ message: 'All puzzles deleted successfully' });
+            }
+            else {
+                res.status(401).json({ message: 'Unauthorized' });
+            }
+        } catch (error) {
+            res.status(500).json({ message: 'Error deleting puzzles', error });
+        }
+    }
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const cors = require('cors');
+require('dotenv').config();
+const cron = require('cron');
+const { newPuzzle } = require('./controllers/puzzle-controller');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +20,28 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+function putPuzzleInDB() {
+  const req = {
+    body: {
+      "password": process.env.SPASSWORD
+    }
+  };
+  const res = {
+    status: function (status) {
+      this.status = status;
+      return this;
+    },
+    json: function (json) {
+      this.json = json;
+      return this;
+    }
+  };
+  newPuzzle(req, res);
+}
+
+const job = new cron.CronJob('0 0 * * *', putPuzzleInDB);
+job.start();
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
