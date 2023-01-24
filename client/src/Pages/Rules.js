eec5ab972c983e35ca41a799cc571457fa2ed003
Bee <xronsredstone@gmail.com>
2023-01-24 13:44:29 -0500

    0.6.1

--- client/src/Pages/Rules.js
+++ client/src/Pages/Rules.js
@@ -17,7 +17,9 @@ function AboutUs() {
         <li>Each row and or column must contain all of the numbers from 1 to 9, with no repetitions.</li>
         <li>Each of the 9 3x3 sub-grids must ALSO contain all of the numbers from 1 to 9, with no repetitions.</li>
         <li>When you click on a number a Modal will pop up with a keypad. Click on the keypad to fill in the number.</li>
-        <li>Press the R key durring the game to reset the puzzle.</li>
+        <li>Press the R key or click the Reset button durring the game to reset the puzzle.</li>
+        <li>Press the N key or click the Notes button durring the game to toggle between filling in the numbers (blue) and notes (marked in purple).</li>
+        <li>Notes are only visible to you and will be checked by the game.</li>
         <li>When you are done filling in the puzzle the game will automatically check if you have won or lost.</li>
         <li>Every day a new puzzle will be generated for you to solve.</li>
         <li>Everyone in the world has the same puzzle at the same time so have fun competing with your friends!</li>
