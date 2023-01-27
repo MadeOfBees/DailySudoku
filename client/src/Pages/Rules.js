eed64640892256797af2d01fb3e7e9e67c9846ef
Bee <xronsredstone@gmail.com>
2023-01-27 17:38:36 -0500

    typoFix

--- client/src/Pages/Rules.js
+++ client/src/Pages/Rules.js
@@ -19,7 +19,7 @@ function AboutUs() {
         <li>When you click on a number a Modal will pop up with a keypad. Click on the keypad to fill in the number or press the number on your keyboard, you can buffer these inputs (like in a fighting game) to get even faster scores.</li>
         <li>Press the R key or click the Reset button durring the game to reset the puzzle.</li>
         <li>Press the M key or click the Pencil button to toggle between filling in numbers as anwsers with pen mode (marked as blue) or notes with the pencil (marked in orange).</li>
-        <li>Press the E key or click the Erase button to use the Eraser. </li>
+        <li>Press the E key or click the eraser button to use the Eraser. </li>
         <li>Notes are only visible to you and will NOT be checked by the game.</li>
         <li>When you are done filling in the puzzle the game will automatically check if you have won or lost.</li>
         <li>Every day a new puzzle will be generated for you to solve.</li>
