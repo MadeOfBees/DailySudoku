f3cd83376a055256f5cc8b2e8602a3450a37d447
Bee <xronsredstone@gmail.com>
2023-01-30 14:23:01 -0500

    cleaned leaderBoard

--- client/src/Pages/LeaderBoard.js
+++ client/src/Pages/LeaderBoard.js
@@ -22,16 +22,34 @@ const LeaderBoard = () => {
         fetchScores('api/scores/top')
             .then((data) => {
                 const formatedScores = data.scores.map((score) => {
-                    const time = (score.time > 60 ? (score.time / 60).toFixed(0) : '0')+ ':' + ((score.time % 60) < 10 ? '0' + (score.time % 60) : (score.time % 60));
-                    return { ...score, time };
+                    const time = score.time;
+                    let minutes = Math.floor(time / 60);
+                    if (minutes < 10) {
+                        minutes = `0${minutes}`;
+                    }
+                    let secondsLeft = time % 60;
+                    if (secondsLeft < 10) {
+                        secondsLeft = `0${secondsLeft}`;
+                    }
+                    const cellOutput = `${minutes}:${secondsLeft}`;
+                    return { time: cellOutput };
                 });
                 setTopAllTime(formatedScores);
             });
         fetchScores('api/scores/todaysTop')
             .then((data) => {
                 const formatedScores = data.scores.map((score) => {
-                    const time = (score.time > 60 ? (score.time / 60).toFixed(0) : '0')+ ':' + ((score.time % 60) < 10 ? '0' + (score.time % 60) : (score.time % 60));
-                    return { ...score, time };
+                    const time = score.time;
+                    let minutes = Math.floor(time / 60);
+                    if (minutes < 10) {
+                        minutes = `0${minutes}`;
+                    }
+                    let secondsLeft = time % 60;
+                    if (secondsLeft < 10) {
+                        secondsLeft = `0${secondsLeft}`;
+                    }
+                    const cellOutput = `${minutes}:${secondsLeft}`;
+                    return { time: cellOutput };
                 });
                 setTopToday(formatedScores);
             });
@@ -42,38 +60,49 @@ const LeaderBoard = () => {
             return;
         }
         const scores = JSON.parse(localStorage.getItem('scores'));
-      const formatedScores = scores.map((score) => {
-            const time = (score.time / 60).toFixed(0) + ':' + ((score.time % 60) < 10 ? '0' + (score.time % 60) : (score.time % 60));
-            return { ...score, time };
+        const formatedScores = scores.map((score) => {
+            console.log(score);
+            score = parseInt(score.time);
+            let minutes = Math.floor(score / 60);
+            if (minutes < 10) {
+                minutes = `0${minutes}`;
+            }
+            let secondsLeft = score % 60;
+            if (secondsLeft < 10) {
+                secondsLeft = `0${secondsLeft}`;
+            }
+            const cellOutput = `${minutes}:${secondsLeft}`;
+            return { time: cellOutput };
         });
         setTopLocal(formatedScores);
     }, []);
 
-      return (
-        (topAllTime.length > 0) ? (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '1rem' }}>
-          <Typography variant="h4" style={{ marginBottom: '1rem' }}>The Leaderboard:</Typography>
-          <Paper style={{ padding: '1rem', backgroundColor: theme.palette.mode === 'light' ? '#e8eced' : theme.palette.background.default }}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell><Typography variant="h6">Best of All Time</Typography></TableCell>
-                  <TableCell><Typography variant="h6">Best of Today</Typography></TableCell>
-                  <TableCell><Typography variant="h6">Your best times</Typography></TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {[...Array(10)].map((_, index) => (
-                  <TableRow key={index}>
-                    <TableCell><Typography variant="body2">{topAllTime[index] ? topAllTime[index].time : ''}</Typography></TableCell>
-                    <TableCell><Typography variant="body2">{topToday[index] ? topToday[index].time : ''}</Typography></TableCell>
-                    <TableCell><Typography variant="body2">{topLocal[index] ? topLocal[index].time : ''}</Typography></TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>
-        </div>
+
+    return (
+        (topAllTime.length > 0 || topToday.length > 0 || topLocal.length > 0) ? (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '1rem' }}>
+                <Typography variant="h4" style={{ marginBottom: '1rem' }}>The Leaderboard:</Typography>
+                <Paper style={{ padding: '1rem', backgroundColor: theme.palette.mode === 'light' ? '#e8eced' : theme.palette.background.default }}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell><Typography variant="h6">Best of All Time</Typography></TableCell>
+                                <TableCell><Typography variant="h6">Best of Today</Typography></TableCell>
+                                <TableCell><Typography variant="h6">Your best times</Typography></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {[...Array(10)].map((_, index) => (
+                                <TableRow key={index}>
+                                    <TableCell><Typography variant="body2">{topAllTime[index] ? topAllTime[index].time : ''}</Typography></TableCell>
+                                    <TableCell><Typography variant="body2">{topToday[index] ? topToday[index].time : ''}</Typography></TableCell>
+                                    <TableCell><Typography variant="body2">{topLocal[index] ? topLocal[index].time : ''}</Typography></TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </div>
         ) : null
     );
 };
